3a6cec95bae2c03bffba28ca3bb4633a685616c6
JosephDevArt <jbdevart@gmail.com>
2020-02-27 21:52:03 +0300

    Updates

---
+++ precache-manifest.010d600eef7fc4254f4940465e896b49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34560cbec8ca6148425514b6f44dc28f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9566382d96b4ce4381bf",
+    "url": "./static/css/main.252c3c29.chunk.css"
+  },
+  {
+    "revision": "a71e6d8a5bc85ace0a99",
+    "url": "./static/js/2.8b028e3f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.8b028e3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9566382d96b4ce4381bf",
+    "url": "./static/js/main.38571068.chunk.js"
+  },
+  {
+    "revision": "b5f95eaa81acdd03d2d2",
+    "url": "./static/js/runtime-main.1c21abc2.js"
+  }
+]);
