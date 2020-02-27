f4b08d5a66d02f183e47e2d674075b632222b2c4
JosephDevArt <jbdevart@gmail.com>
2020-02-27 21:52:33 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+
+import "./App.scss";
+import CurrensyRow from "./CurrensyRow";
+
+const BASE_URL = "https://api.exchangeratesapi.io/latest";
 
 function App() {
+  const [currencyOptions, setCurrencyOptions] = useState([]);
+  const [fromCurrency, setFromCurrency] = useState();
+  const [toCurrency, setToCurrency] = useState();
+  const [exchangeRate, setExchangeRate] = useState();
+  const [amount, setAmount] = useState(1);
+  const [amountInFromCurrency, setAmountInFromCurrency] = useState(true);
+  let toAmount, fromAmount;
+  if (amountInFromCurrency) {
+    fromAmount = amount;
+    toAmount = amount * exchangeRate;
+  } else {
+    toAmount = amount;
+    fromAmount = amount / exchangeRate;
+  }
+  useEffect(() => {
+    fetch(BASE_URL)
+      .then(response => response.json())
+      .then(data => {
+        const firstCurrency = Object.keys(data.rates)[0];
+        setCurrencyOptions([data.base, ...Object.keys(data.rates)]);
+        setFromCurrency(data.base);
+        setToCurrency(firstCurrency);
+        setExchangeRate(data.rates[firstCurrency]);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (fromCurrency != null && toCurrency != null) {
+      fetch(`${BASE_URL}?base=${fromCurrency}&symbols=${toCurrency}`)
+        .then(response => response.json())
+        .then(data => setExchangeRate(data.rates[toCurrency]));
+    }
+  }, [fromCurrency, toCurrency]);
+
+  function handleFromAmountChange(event) {
+    setAmount(event.target.value);
+    setAmountInFromCurrency(true);
+  }
+  function handleToAmountChange(event) {
+    setAmount(event.target.value);
+    setAmountInFromCurrency(false);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Converter</h1>
+      <CurrensyRow
+        selectedCurrency={fromCurrency}
+        currencyOptions={currencyOptions}
+        onChangeCurrency={e => setFromCurrency(e.target.value)}
+        amount={Math.round(fromAmount * 100) / 100}
+        onChangeAmount={handleFromAmountChange}
+      />
+      <div className="equals">=</div>
+      <CurrensyRow
+        selectedCurrency={toCurrency}
+        currencyOptions={currencyOptions}
+        onChangeCurrency={e => setToCurrency(e.target.value)}
+        amount={Math.round(toAmount * 100) / 100}
+        onChangeAmount={handleToAmountChange}
+      />
     </div>
   );
 }
---
+++ src/CurrensyRow.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+function CurrensyRow(props) {
+  const {
+    currencyOptions,
+    selectedCurrency,
+    onChangeCurrency,
+    amount,
+    onChangeAmount
+  } = props;
+  //   Initially amount equals to NaN, wait untill it's assigned to number
+  if (amount) {
+    return (
+      <>
+        <input
+          className="input"
+          type="number"
+          value={amount}
+          onChange={onChangeAmount}
+        />
+        <select value={selectedCurrency} onChange={onChangeCurrency}>
+          {currencyOptions.map(option => (
+            <option key={option} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
+      </>
+    );
+  } else {
+    return <div>Loading...</div>;
+  }
+}
+
+export default CurrensyRow;
